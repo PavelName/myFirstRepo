7176e5b45b7f1f518c10a25fb6d71634613a15c8
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2022-04-02 13:32:48 +0800

    доделал выводы в консоль урок 1

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let title = 'project';
 let screens = 'simple, complex, interactive';
-let screenPrice = '45';
+let screenPrice = 4.500;
+let rollback = 76;
+let fullPrice = 15000;
+let adaptive = true;
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(screenPrice);
+console.log(fullPrice);
+console.log(screens.toLowerCase());
+console.log(screens.split());
+console.log(fullPrice * (rollback / 100));
 
 
