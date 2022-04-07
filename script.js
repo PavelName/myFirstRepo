4b223bfa7005b49fd7f8d261e9ed9b63b357c3ea
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2022-04-07 19:09:18 +0800

    Урок №3

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-let title = 'project';
-let screens = 'simple, complex, interactive';
-let screenPrice = 4.500;
-let rollback = 76;
-let fullPrice = 15000;
-let adaptive = true;
+'use strict';
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log(screenPrice);
+let title = prompt('Как называется ваш проект?');
+console.log('type title:', typeof title);
+
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+console.log('type screens:', typeof screens);
+
+let screenPrice = prompt('Сколько будет стоить данная работа?', '8000, 12000,15000 рублей');
+console.log('type screenPrice:', typeof screens);
+console.log(parseFloat(screenPrice));
+
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+console.log('type adaptive', typeof adaptive);
+
+let service1 = prompt('Каой дополнительный тип услуг нужен?', 'Ремонт');
+let servicePrice1 = prompt('Сколько это будет стоить?', '8000, 12000,15000 рублей');
+console.log(parseFloat(servicePrice1));
+
+let service2 = prompt('Каой дополнительный тип услуг нужен?', 'Тестирование');
+let servicePrice2 = prompt('Сколько это будет стоить?', '8000, 12000,15000 рублей');
+console.log(parseFloat(servicePrice2));
+
+let fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
 console.log(fullPrice);
-console.log(screens.toLowerCase());
-console.log(screens.split());
-console.log(fullPrice * (rollback / 100));
 
+let rollback = 50;
+let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+console.log(Math.ceil(servicePercentPrice));
 
+if (fullPrice > 30000) {
+    console.log('Делаем скидку 10%');
+} else if (fullPrice > 15000 && fullPrice < 30000) {
+    console.log('Делаем скидку 5%');
+} else if (fullPrice < 15000 && fullPrice > 0) {
+    console.log('Скидка не предусмотрена');
+} else if (fullPrice < 0) {
+    console.log('Что то пошло не так!')
+}
